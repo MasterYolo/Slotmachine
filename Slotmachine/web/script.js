b37f7e853d3c2984980b8ffd4287fd9c78916e25
Kryptering-examensarbete <forhelvetemads@gmail.com>
2014-08-20 15:48:50 +0200

    *Fixed so the animation of the three slots so it stops at the right time.

--- Slotmachine/web/script.js
+++ Slotmachine/web/script.js
@@ -22,20 +22,18 @@ angular.module('ControllerModule', []).
                 $scope.imgsrc5 = "http://www.nonstop-games.com/examples/slot/img/build-64.png";
                 $scope.imgsrc6 = "http://www.nonstop-games.com/examples/slot/img/goods-64.png";
             }]);
-        
+
 /* Animation part */
 $(function() {
     var positions = new Object();
     positions.lotf = 0;
-    positions.gardenparty = 80;
-    positions.kendalcalling = 175;
-    positions.vfestival = 260;
-    positions.creamfields = 350;
-    positions.readingleeds = 450;
-    positions.bestival = 540;
+    positions.hammer = 80;
+    positions.dollar = 175;
+    positions.bolt = 260;
+    positions.gold = 350;
+    positions.box = 450;
+    positions.goldhelm = 540;
 
-    var scope = angular.element("#slot1").scope();
-    
     var one = new Slot('.slot#one', 20, 10, 'up', 700);
     var two = new Slot('.slot#two', 13, 7, 'down', 800);
     var three = new Slot('.slot#three', 17, 8, 'up', 600);
@@ -52,56 +50,86 @@ $(function() {
     });
 });
 
-function Slot(slot, maxSpeed, timer) {
-
-
+function Slot(slot, maxSpeed, initialSpeed, direction, timer) {
     this.slot = slot;
-    this.speed = 0;
+    this.speed = initialSpeed;
     this.maxSpeed = maxSpeed;
+    this.direction = direction;
     this.timer = timer;
     this.interval = null;
-
-    var self = this;
-
+    
+    var that = this;
+    
     this.go = function() {
         var $slot = $(this.slot);
         $slot.spStart();
 
         this.interval = window.setInterval(function() {
-            var step = Math.floor(Math.random() * self.speed + 1);
-            if (self.speed < (self.maxSpeed)) {
-                self.speed += step;
+            var step = Math.floor(Math.random() * that.speed + 1);
+            if (that.speed < (that.maxSpeed)) {
+                that.speed += step;
             }
 
-            $slot.spSpeed(self.speed);
-        }, self.timer);
+            $slot.spSpeed(that.speed);
+        }, that.timer);
     };
-    
     
     this.stop = function(position) {
         var $slot = $(this.slot);
-        var step = self.speed / 5;
+        var step = this.speed / 5;
         var counter = 0;
-
+        
         $slot.spSpeed(this.maxSpeed);
         $slot.spStart();
-
+        
         this.interval = window.setInterval(function() {
             counter++;
-            self.speed -= step;
-            $slot.spSpeed(self.speed);
-
+            that.speed -= step;
+            $slot.spSpeed(that.speed);
+            
             if (counter == 3) {
                 $slot.spSpeed(1);
-                clearInterval(self.interval);
+                clearInterval(that.interval);
+                land(position);
                 $slot.spSpeed(0);
                 $slot.spStop();
-                self.speed = 0;
+                that.speed = 0;
             }
-        }, self.timer);
+        }, that.timer);
     };
-
-    $(self.slot).pan({fps: '30', dir: 'down'});
-    $(self.slot).spStop();
+    
+    /* private */
+    var land = function(position) {
+        var $slot = $(that.slot);
+        var element_id = $slot.attr('id');
+        var slotImage, slotImageHeight, repeated, actualPosition;
+        backgroundPos = document.getElementById(element_id).style.backgroundPosition;
+        backgroundPos = backgroundPos.split(' ')[1];
+        backgroundPos = parseInt(backgroundPos, 10);
+        
+        // get height of the slots background image
+        slotImage = new Image();
+        slotImage.src = $slot.css('background-image').replace(/"/g, '').replace(/url\(|\)$/ig, '');
+        slotImageHeight = slotImage.height;
+        
+        repeated = parseInt(backgroundPos / slotImageHeight, 10);
+        
+        actualPosition = (slotImageHeight * repeated) - position; 
+        if (that.direction == 'down') {
+            while (actualPosition < backgroundPos) {
+                actualPosition += slotImageHeight;
+            }
+        }
+        
+        if (that.direction == 'up') {
+            while (actualPosition > backgroundPos) {
+                actualPosition -= slotImageHeight;
+            }
+        }
+        
+        $slot.animate({ backgroundPosition: '0 ' + actualPosition + 'px' }, 1000);
+    }
+    
+    $(this.slot).pan({ fps: '30', speed: this.speed, dir: this.direction });
+    $(this.slot).spStop();
 }
-/* End of animation part */
